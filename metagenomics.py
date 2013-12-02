de672e39cd0c6d73a0f18d85213941992b865c75
talmo <talmo.pereira@umbc.edu>
2013-12-01 21:02:45 -0500

    Big Turkey break commit!
    - Added __init__.py so metagenomics can be imported from other scripts
    - Added a LOT of utility functions to metagenomics.py. They are all
    thoroughly commented and have examples of usage.
    - Commented perm_analysis.py more thoroughly and updated to be
    consistent with changes in gpu_pssm and metagenomics.
    - *NEW*: A (fully commented) parameters section!

--- metagenomics.py
+++ metagenomics.py
@@ -7,9 +7,12 @@ Created on Sun Nov 24 00:48:00 2013
 import numpy as np
 import os
 
-# Constants
+##### Constants #####
+# The nucleotide dictionary used in all the functions.
 bases = "ACGT"
 
+
+##### Methods #####
 
 def load_scaffolds(filename):
     """
@@ -20,38 +23,72 @@ def load_scaffolds(filename):
         TACTCTGGAAGGAGATATT...
     The scaffold is a reconstruction of one or more contigs (contiguous reads).
     
+    This function also supports files that have multiple lines of sequence data
+    per scaffold, i.e.:
+        >gi|49175990|ref|NC_000913.2| Escherichia coli str. K-12 substr. MG1655 chromosome, complete genome
+        AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC
+        TTCTGAACTGGTTACCTGCCGTGAGTAAATTAAAATTTTATTGACTTAGGTCACTAAATACTTTAACCAA
+        ...
+    
     Args:
         filename: The path to the file containing the sequence data.
     
     Returns:
-        sequence: The entire patient file loaded into one array. This is a
-            uint8 NumPy array where A = 0, C = 1, G = 2, T = 3.
+        sequence: The entire patient file loaded into one array. This is an
+            integer numpy array where A = 0, C = 1, G = 2, T = 3.
         scaffolds: Since all the scaffolds are joined into one array, this is
             a list of the indices where each scaffold begins. This can be used
             to keep track of which segments of the sequence are contiguous.
+            
+    Example:
+        >>> sequence, scaffolds = load_scaffolds("../NC_000913.fna") # The E. coli genome
+        >>> sequence
+        array([0, 2, 1, ..., 3, 3, 1])
+        >>> sequence.dtype
+        dtype('int32')
+        >>> scaffolds
+        array([0])
+        >>> scaffolds.dtype
+        dtype('int32')
+        >>> int2nt(sequence[0:70]) # Display as a string
+        'AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC'
+        
+    Confirm that the first line is being read correctly:
+        >>> ground_truth = open("../NC_000913.fna").readlines(1)[1].strip() # Read the first line
+        >>> sequence, scaffolds = load_scaffolds("../NC_000913.fna")
+        >>> hypothesis = int2nt(sequence[0:70])
+        >>> hypothesis
+        'AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC'
+        >>> hypothesis == ground_truth
+        True
         
     """
-    scaffolds = []
+    scaffolds = np.array([], dtype=np.int32)
     
     with open(filename, "r+") as f:
         # Pre-allocate sequence to size of file
         f.seek(0, os.SEEK_END)
         size = f.tell()
         f.seek(0, os.SEEK_SET)
-        sequence = np.tile(-1, size)
+        sequence = np.tile(np.array([-1], dtype=np.int32), size)
         
         # Process the lines in the file
         pos = 0
+        next_line_starts_scaffold = False
         for line in f:
-            if ">" not in line:
+            if ">" in line:
+                next_line_starts_scaffold = True
+            else:
                 # Keep sequence as the charcode integers
                 scaffold_seq = np.array(line.strip(), "c").view(np.uint8)
                 
                 # Replace the sequence chunk in memory
                 sequence[pos:pos + len(scaffold_seq)] = scaffold_seq
-                             
-                # Keep track of the beginning of scaffolds
-                scaffolds.append(pos)
+                
+                if next_line_starts_scaffold:
+                    # Keep track of the beginning of scaffolds
+                    scaffolds = np.append(scaffolds, pos)
+                    next_line_starts_scaffold = False
                 
                 # Update position in the sequence array
                 pos += len(scaffold_seq)
@@ -66,9 +103,331 @@ def load_scaffolds(filename):
     return sequence, scaffolds
 
 
-def permute_pssm(pssm):
-    # Break PSSM into columns
-    pssm_cols = pssm.reshape((pssm.size / 4, 4))
+def pssm2cols(pssm):
+    """
+    Returns the PSSM as a 2-dimensional array such that each 4 element array
+    is a column vector of the PSSM, that is, each element in the first
+    dimension represents a column of the PSSM, and each element in the second
+    dimension represents the scores for each nucleotide.
     
-    # Shuffle columns and return flattened array
-    return np.random.permutation(pssm_cols).flatten()
+    This can be used for any matrix structure, not just PSSMs, however.
+
+    The reverse of this function is simply pssm2cols(pssm).flatten().
+    
+    Args:
+        pssm: An array of size 4 * w, where w is the width of the PSSM, or the
+            number of columns it has.
+    
+    Returns:
+        pssm_cols: A two dimensional array containing the values of pssm.
+    
+    Example:
+        >>> cols = 4
+        >>> pssm = np.array(range(0, 4 * cols))
+        >>> pssm
+        array([ 0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15])
+        >>> pssm2cols(pssm)
+        array([[ 0,  1,  2,  3],
+               [ 4,  5,  6,  7],
+               [ 8,  9, 10, 11],
+               [12, 13, 14, 15]])
+       
+       For a more readable representation, using print_pssm() which breaks the
+       pssm into columns automatically if necessary:
+       >>> print_pssm(pssm)
+            |      0 |      1 |      2 |      3 |
+        -----------------------------------------
+        | A |  0.000 |  4.000 |  8.000 | 12.000 |
+        | C |  1.000 |  5.000 |  9.000 | 13.000 |
+        | G |  2.000 |  6.000 | 10.000 | 14.000 |
+        | T |  3.000 |  7.000 | 11.000 | 15.000 |
+        -----------------------------------------
+    """
+    pssm_cols = pssm.reshape((pssm.size / 4, 4))
+    return pssm_cols
+
+
+def permute_pssm(pssm):
+    """
+    Randomly shuffles the *columns* of the PSSM. This means that the value of
+    each base is the same, but the positions are different.
+    
+    See the example for a clearer picture of how this works.
+    
+    Args:
+        pssm: A 1D or 2D numpy array representing the PSSM.
+    
+    Returns:
+        perm_pssm: The column-permuted pssm. This will maintain the dimensions
+            of the PSSM, so if a 1D array was inputted, a 1D array is returned.
+        
+    Example:
+        >>> cols = 4
+        >>> pssm = np.array(range(0, 4 * cols))  # Generate a naive PSSM
+        >>> print_pssm(pssm)
+            |      0 |      1 |      2 |      3 |
+        -----------------------------------------
+        | A |  0.000 |  4.000 |  8.000 | 12.000 |
+        | C |  1.000 |  5.000 |  9.000 | 13.000 |
+        | G |  2.000 |  6.000 | 10.000 | 14.000 |
+        | T |  3.000 |  7.000 | 11.000 | 15.000 |
+        -----------------------------------------
+        >>> pssm = permute_pssm(pssm)
+        >>> print_pssm(pssm)
+            |      0 |      1 |      2 |      3 |
+        -----------------------------------------
+        | A | 12.000 |  4.000 |  8.000 |  0.000 |
+        | C | 13.000 |  5.000 |  9.000 |  1.000 |
+        | G | 14.000 |  6.000 | 10.000 |  2.000 |
+        | T | 15.000 |  7.000 | 11.000 |  3.000 |
+        -----------------------------------------
+    """
+    _pssm = pssm
+    if len(pssm.shape) == 1:
+        # Break PSSM into columns if it is flat (i.e., 1D)
+        _pssm = pssm2cols(pssm)
+        
+    # Shuffle columns of the PSSM
+    perm_pssm = np.random.permutation(_pssm)
+    
+    if len(pssm.shape) == 1:
+        # Re-flatten if it was originally a 1D array
+        perm_pssm = perm_pssm.flatten()
+    
+    return perm_pssm
+
+
+def print_pssm(pssm, max_width=130):
+    """
+    Prints the PSSM in tabular form that is easily readable.
+    
+    Args:
+        pssm: A 1- or 2-dimensional numpy array of PSSM values.
+            Note: If the array is flat, or 1-dimensional, it will be converted
+            internally to a 2d array, so it does not need to be broken into
+            columns before being passed to the function.
+        
+    Example:
+        >>> cols = 4
+        >>> pssm = np.random.uniform(-7.5, 2.0, 4 * cols) # 1D array
+        >>> print_pssm(pssm)
+            |      0 |      1 |      2 |      3 |
+        -----------------------------------------
+        | A | -1.517 | -4.536 | -5.642 | -1.505 |
+        | C | -7.157 |  0.742 | -3.313 | -3.493 |
+        | G | -3.226 | -5.244 |  1.085 | -1.207 |
+        | T | -2.886 | -4.886 |  1.919 | -4.437 |
+        -----------------------------------------
+    """
+    # Convert to 2D array if needed
+    _pssm = pssm
+    if len(pssm.shape) == 1:
+        _pssm = pssm2cols(pssm)
+    cols = int(_pssm.shape[0])
+    rows = int(_pssm.shape[1])    
+    
+    # Calculate in how many rows we'll display the output
+    cols_per_output = int(max_width - 4) / 9
+    
+    for metacol in range(0, cols, cols_per_output):
+        # Table header
+        print " " * 3, # Nucleotide label column
+        for col in range(metacol, min(metacol + cols_per_output, cols)):
+            print "| %6d" % col,
+        print "|"
+        print "-" * (9 * min(cols_per_output, min(metacol + cols_per_output, cols) - metacol) + 5)
+        
+        # Table body
+        for row in range(rows):
+            print "|", bases[row],
+            for col in range(metacol, min(metacol + cols_per_output, cols)):
+                print "| %6.3f" % (_pssm[col][row]),
+            print "|"
+            
+        print "-" * (9 * min(cols_per_output, min(metacol + cols_per_output, cols) - metacol) + 5)
+
+
+def int2nt(int_seq):
+    """
+    Returns the string representation of a sequence from an integer array of
+    nucleotides.
+    
+    The dictionary for this is at the top of this file and defaults to:
+        bases = "ACGT"
+    So A = 0, C = 1, G = 2, T = 3.
+
+    The inverse of the function is nt2int().
+    
+    Args:
+        int_seq: A 1D array of integer representations of nucleotides.
+    
+    Returns:
+        seq: A string representation of the nucleotide sequence.
+        
+    Example:
+        >>> sequence = np.random.randint(0, 3, 20) # Generate random 20 nucleotide sequence
+        >>> sequence
+        array([2, 1, 0, 2, 2, 2, 2, 0, 0, 1, 0, 2, 0, 1, 0, 1, 2, 0, 1, 2])
+        >>> int2nt(sequence)
+        'GCAGGGGAACAGACACGACG'
+        
+    Confirm that the function can convert the sequence reversibly:
+        >>> np.array_equal(nt2int(int2nt(sequence)), sequence)
+        True
+    """
+    seq = "".join([bases[b] for b in int_seq])
+    return seq
+
+
+def nt2int(nt_seq):
+    """
+    Returns an integer representation of a string sequence of nucleotides.
+    
+    This function is the inverse of int2nt().
+    
+    Args:
+        nt_seq: A string representation of the nucleotide sequence.
+    
+    Returns:
+        int_seq: A 1D array of integer representations of nucleotides.
+        
+    Example:
+        >>> from random import choice
+        >>> sequence = "".join([choice(bases) for _ in range(20)]) # Generate random 20 nucleotide sequence
+        >>> sequence
+        'CTTTCCCAAGTCTAGGGTGT'
+        >>> nt2int(sequence)
+        array([1, 3, 3, 3, 1, 1, 1, 0, 0, 2, 3, 1, 3, 0, 2, 2, 2, 3, 2, 3])
+        >>> nt2int(sequence).dtype
+        dtype('int32')
+        
+    Confirm that the function converts the sequence reversibly:
+        >>> int2nt(nt2int(sequence)) == sequence
+        True
+    """
+    # Convert the string into an array of characters and then as integers
+    int_seq = np.array(nt_seq, "c").view(np.uint8).astype(np.int32)
+    
+    # Rescale the array of character codes to [0, 3]
+    for value, base in enumerate(bases):
+        int_seq[np.where(int_seq == ord(base))] = value
+    
+    return int_seq
+    
+    
+def consensus(pssm):
+    """
+    Returns the consensus sequence of a PSSM.
+    
+    This is the sequence that has the best score for a given PSSM.
+    
+    Args:
+        pssm: A 1D or 2D numpy array representing the PSSM.
+    
+    Returns:
+        seq: An integer sequence representing the consensus of the PSSM.
+    
+    Example:
+        >>> cols = 4
+        >>> pssm = np.random.uniform(-7.5, 2.0, 4 * cols)
+        >>> print_pssm(pssm)
+            |      0 |      1 |      2 |      3 |
+        -----------------------------------------
+        | A | -7.021 | -5.289 |  1.621 | -3.932 |
+        | C | -3.224 | -6.341 | -4.706 |  1.788 |
+        | G | -3.844 | -3.741 | -1.344 | -4.757 |
+        | T | -3.157 | -2.437 | -7.475 | -4.794 |
+        -----------------------------------------
+        >>> consensus(pssm)
+        array([3, 3, 0, 1])
+        >>> int2nt(consensus(pssm))
+        'TTAC'
+    """
+    # Break PSSM into columns
+    _pssm = pssm    
+    if len(pssm.shape) == 1:
+        _pssm = pssm2cols(pssm)
+        
+    seq = np.array([np.argmax(col) for col in _pssm], dtype=np.int32)
+    return seq
+
+
+def rev_pssm(pssm):
+    """
+    Returns the reverse-complement of a PSSM based on the Watson-Crick model of
+    nucleotide pairing.
+    
+    Reversed PSSMs can be to score a sequence of nucleotides as if it were on
+    the reverse strand.
+    
+    Args:
+        pssm: A 1D or 2D numpy array representing the PSSM.
+    
+    Returns:
+        rev_pssm: A 1D or 2D numpy array representing the reverse-complement of
+            the PSSM.
+    
+    Example:
+        >>> cols = 4
+        >>> pssm = np.array(range(0, 4 * cols)) # Generate a naive PSSM
+        >>> print_pssm(pssm)
+            |      0 |      1 |      2 |      3 |
+        -----------------------------------------
+        | A |  0.000 |  4.000 |  8.000 | 12.000 |
+        | C |  1.000 |  5.000 |  9.000 | 13.000 |
+        | G |  2.000 |  6.000 | 10.000 | 14.000 |
+        | T |  3.000 |  7.000 | 11.000 | 15.000 |
+        -----------------------------------------
+        >>> r_pssm = rev_pssm(pssm)
+        >>> print_pssm(r_pssm)
+            |      0 |      1 |      2 |      3 |
+        -----------------------------------------
+        | A |  3.000 |  2.000 |  1.000 |  0.000 |
+        | C |  4.000 |  3.000 |  2.000 |  1.000 |
+        | G |  5.000 |  4.000 |  3.000 |  2.000 |
+        | T |  6.000 |  5.000 |  4.000 |  3.000 |
+        -----------------------------------------
+    """
+    # Convert to 1D array if needed
+    _pssm = pssm    
+    if len(pssm.shape) == 2:
+        _pssm = _pssm.flatten()
+    
+    # Get the reverse-complement
+    _rev_pssm = np.array([_pssm[i / 4 + (3 - (i % 4))] for i in range(_pssm.size)][::-1])
+    return _rev_pssm
+
+
+def score_site(sequence, pssm):
+    """
+    Scores a sequence against a PSSM.
+    
+    This function is not optimized for large sequences, nor does it score the
+    reverse strand of the sequence. If used on a large sequence, it will
+    consume a very large amount of memory!
+    
+    Use it as a convenience function for scoring individual sites on a genome.
+    
+    Note that the width of the PSSM (the number of columns) must be equal to
+    the length of the sequence!
+    
+    Args:
+        sequence: A 1D array of integer representations of nucleotides.
+        pssm: A 1D or 2D numpy array representing the PSSM.
+    
+    Returns:
+        score: The number calculated by summing up the scores for each base at
+            each position in the sequence.
+
+    Example:
+        >>> cols = 4
+        >>> seq = np.random.randint(0, 3, cols) # Generate random sequence
+        >>> pssm = np.random.uniform(-7.5, 2.0, 4 * cols) # Generate random PSSM
+        >>> score_site(seq, pssm)
+        -14.142570531696526
+        >>> score_site(consensus(pssm), pssm) # The best possible score for this PSSM
+        -2.5526269119209908
+    """
+    score = sum([pssm[i * 4 + j] for i, j in enumerate(sequence)])
+    return score
+
--- perm_analysis.py
+++ perm_analysis.py
@@ -3,25 +3,85 @@
 Created on Sun Nov 24 00:50:44 2013
 
 @author: Talmo
-"""
 
-from gpu_pssm import gpu_pssm
-import metagenomics as mg
+This script does a permutation analysis on the scores of the LexA binding motif
+in the MetaHit database in order to support the findings in Cornish et al.
+(2013).
+
+Dependencies:
+    - Numpy
+    - Matplotlib
+    - gpu_pssm (https://github.com/ErillLab/gpu_pssm)
+    - MetaHit database (Qin et al., 2010; see comments below)
+"""
+import sys
+import os.path
 import glob
 import numpy as np
 import matplotlib.pyplot as plt
+import metagenomics as mg
+# Import gpu_pssm from the parent directory
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+from gpu_pssm import gpu_pssm
+
 
 def main():
-    # Parameters
-    metahit_path = "./Metahit"
-    binding_sites_path = "./lexA.seq.fa"
-    score_threshold = -100
-    permutations = 10
-    bins = range(-100, 24) # See figure S4   
+    ##### Parameters #####
+    # Path to the MetaHit database.
+    # The patient files can be downloaded from:
+    #   http://www.bork.embl.de/~arumugam/Qin_et_al_2010/
+    # Make sure these are extracted from their packages! (i.e.: "gunzip *.gz")
+    # Note: After extraction, the 84 patient files occupy a total of 6.56 GB on
+    # disk!
+    # The original paper can be found at:
+    #   http://www.nature.com/nature/journal/v464/n7285/full/nature08821.html
+    metahit_path = "../MetaHit"
+    
+    # The collection of binding sites to generate the PSSM from.
+    # LexA.seq.fa is a collection of 115 experimentally-determined binding
+    # sites reported in literature. See Table S1 in Cornish et al. (2013).
+    binding_sites_path = "./LexA.seq.fa"
+    
+    # Number of permutations to run. Note that the COLUMNS of the PSSM
+    permutations = 50
+    
+    # Scores below this number of bits will not be reported.
+    # Lower values will give more (false-positive) results and also slow down
+    # the execution of the program since more memory needs to be allocated to
+    # store the score values.
+    score_threshold = -50.0
+    
+    ### Parameters below this line should *probably* not be changed. ###
+    
+    # The background frequency of the bases. An equiprobable frequency
+    # distribution assumes that each base has an equal probability of occuring,
+    # that is: P(A) = P(C) = P(G) = P(T) = 0.25 => GC-content = 0.5
+    # If set to False, the background frequency will be calculated based on the
+    # nucleotide composition of each patient.
+    # For better comparison across patients, this should be set to True.
+    equiprobable_nuc_freqs = True
+    
+    # The ranges that the scores should be binned into.
+    # Since no scores will be saved below the score_threshold, it serves as a
+    # lower bound to the bins. A good upper bound is around ~30 bits since the
+    # maximum theoretical score a sequence can have from a PSSM is 32.
+    # Under equiprobable frequencies, the LexA consensus sequence has a score
+    # of ~24 bits, so no sequence should score higher than that.
+    bins = range(int(score_threshold), 32, 1)
+    
+    # If you'd like to compare the output of this program by scoring the
+    # E. coli genome, set this to True.
+    # Make sure NC_000913.fna is in the parent directory.
+    # The genome sequence can be download from:
+    #  ftp://ftp.ncbi.nlm.nih.gov/genomes/Bacteria/Escherichia_coli_K_12_substr__MG1655_uid57779/
+    # Use this for comparison/debugging.
+    score_ecoli_instead = False
     ##########
     
-    # Load patient files into memory
+    # Find patient files on disk
     metahit_db = glob.glob(metahit_path + "/MH[0-9]*.seq.fa")
+    if score_ecoli_instead:
+        metahit_db = ["../NC_000913.fna"] # E. coli genome (for debugging)
     
     # For debugging, truncate to just first patient
     metahit_db = [metahit_db[0]]
@@ -29,14 +89,22 @@ def main():
     for patient_file in metahit_db:
         # Load the sequence into memory
         metagenome, scaffolds = mg.load_scaffolds(patient_file)
+        print "Genome size:", metagenome.size, "| Scaffolds:", scaffolds.size
         
-        # Calculate the background nucleotide frequency for the metagenome
-        mg_frequencies = np.bincount(metagenome).astype(np.float) / metagenome.size
+        # Assume equiprobable mononucleotide frequencies
+        mg_frequencies = [0.25] * 4
+        
+        if not equiprobable_nuc_freqs:
+            # Calculate the background nucleotide frequency for the metagenome
+            mg_frequencies = np.bincount(metagenome).astype(np.float) / metagenome.size
         
         # Calculate the original PSSM from binding sites
-        original_pssm = gpu_pssm.create_pssm(binding_sites_path, mg_frequencies)
+        original_pssm = gpu_pssm.create_pssm(binding_sites_path, genome_frequencies = mg_frequencies)
+        print "Unpermuted PSSM:"
+        mg.print_pssm(original_pssm)
         
         # Score the metagenome using the original PSSM
+        print "Scoring without permuting..."
         original_scores = score_patient(metagenome, scaffolds, original_pssm, score_threshold, bins)
         
         # Keep the distributions of the scores
@@ -44,11 +112,8 @@ def main():
                 
         for permutation in range(permutations):
             print "Permutation %d/%d..." % (permutation + 1, permutations)
-            # Break the PSSM into columns
-            pssm_cols = original_pssm.reshape((original_pssm.size / 4, 4))
-            
-            # Shuffle the columns and re-flatten the PSSM
-            pssm = np.random.permutation(pssm_cols).flatten()
+            # Permute the PSSM
+            pssm = mg.permute_pssm(original_pssm)
             
             # Re-score using permuted PSSM
             perm_scores = score_patient(metagenome, scaffolds, pssm, score_threshold, bins)            
@@ -57,11 +122,11 @@ def main():
             patient_scores.append(perm_scores)
     
     # Plot results
-    #print patient_scores
+    plt.figure()
     for score in patient_scores[1:]:
         cdf = np.cumsum(score)
         plt.plot(bins[1:], cdf, "D-r", alpha=0.5, label="Permutation")
-        
+    
     cdf = np.cumsum(patient_scores[0])
     plt.plot(bins[1:], cdf, "D-b", lw=3, label="Original")
     plt.xlabel("Site score (bits)")
@@ -71,6 +136,7 @@ def main():
     plt.grid()
     
     plt.figure()
+    
     for score in patient_scores[1:]:
         plt.plot(bins[1:], score, "D-r", alpha=0.5, label="Permutation")
     plt.plot(bins[1:], patient_scores[0], 'D-b', lw=3, label="Original")
@@ -79,22 +145,29 @@ def main():
     handles, labels = plt.gca().get_legend_handles_labels()
     plt.legend(handles[-2:], labels[-2:], loc="best")
     plt.grid()
-    
+
+
 def score_patient(metagenome, scaffolds, pssm, score_threshold, bins):
     # Score the metagenome with the new PSSM
     scores = gpu_pssm.score_long_sequence(metagenome, pssm, keep_strands=False)
+    #scores, __ = gpu_pssm.score_sequence_with_cpu(metagenome, pssm, benchmark=False)
 
     # Invalidate cross-scaffold scores
     for pos in scaffolds:
         if pos - (pssm.size / 4) + 1 > 0:
             scores[pos - (pssm.size / 4) + 1:pos] = -np.inf
 
-    # Eliminate scores below threshold        
+    # Eliminate scores below threshold
     high_scores = np.where(scores >= score_threshold)
+    #print np.where(scores >= score_threshold)
+    #print scores.size
+    #print len(scaffolds)
+    print "Scores >= 20 bits:", len(np.where(scores >= 20)[0])
+    #print len(high_scores[0])
     scores = scores[high_scores]
     
     # Bin the scores
-    score_hist, __ = np.histogram(scores, bins, density=True)
+    score_hist, __ = np.histogram(scores, bins, density=False)
     
     return score_hist
 
